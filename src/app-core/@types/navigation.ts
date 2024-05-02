95ce8c32bfe498d57f7554befe6860b9762db762
Toan <thaitoan3039015@gmail.com>
2024-05-02 11:24:33 +0700

    chore: change type names

--- src/app-core/@types/navigation.ts
+++ src/app-core/@types/navigation.ts
@@ -1,11 +1,11 @@
-export interface NavigationTree {
-    key: string;
-    path: string;
-    isExternalLink?: boolean;
-    title: string;
-    translateKey: string;
-    icon?: JSX.Element;
-    type: "title" | "collapse" | "item";
-    authority: string[];
-    subMenu: NavigationTree[];
-}
+export type NavigationTree = {
+	key: string;
+	path: string;
+	isExternalLink?: boolean;
+	title: string;
+	translateKey: string;
+	icon?: JSX.Element;
+	type: "title" | "collapse" | "item";
+	authority: string[];
+	subMenu: NavigationTree[];
+};
--- src/app-core/@types/routes.tsx
+++ src/app-core/@types/routes.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { ComponentType, LazyExoticComponent } from "react";
 
-export type IRoute = {
-    key: string;
-    path: string;
-    component: LazyExoticComponent<ComponentType<any>>;
-    authority: string[];
-    crumb?: string;
+export type RouteConfig = {
+	key: string;
+	path: string;
+	component: LazyExoticComponent<ComponentType<any>>;
+	authority: string[];
+	crumb?: string;
 };
 
-export type IRouteLayout = {
-    prefix: string;
-    layout: JSX.Element;
-    children: IRoute[];
-    isPrivate?: boolean;
+export type AppRoutesConfig = {
+	prefix: string;
+	layout: JSX.Element;
+	children: RouteConfig[];
+	isPrivate?: boolean;
 };
