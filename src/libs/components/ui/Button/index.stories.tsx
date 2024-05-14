83cafd014138c92b2ae37098b68ea85f1de4b0af
Toan <thaitoan3039015@gmail.com>
2024-05-14 16:20:05 +0700

    chore: setup storybook

---
+++ src/libs/components/ui/Button/index.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from ".";
+
+const meta: Meta<typeof Button> = {
+	component: Button,
+	title: "Button",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Disable: Story = {
+	args: {
+		disabled: true,
+	},
+};
